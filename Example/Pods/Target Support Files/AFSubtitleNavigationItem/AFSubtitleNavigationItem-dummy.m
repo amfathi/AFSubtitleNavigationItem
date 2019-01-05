2e1692df8609066e65d4ee1b3d985adfa290ef09
Ahmed Fathi <AhmedFathi680@gmail.com>
2019-01-05 13:32:56 +0200

    - add the pods.

---
+++ Example/Pods/Target Support Files/AFSubtitleNavigationItem/AFSubtitleNavigationItem-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_AFSubtitleNavigationItem : NSObject
+@end
+@implementation PodsDummy_AFSubtitleNavigationItem
+@end
---
+++ Example/Pods/Target Support Files/Pods-AFSubtitleNavigationItem_Tests/Pods-AFSubtitleNavigationItem_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_AFSubtitleNavigationItem_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_AFSubtitleNavigationItem_Tests
+@end
